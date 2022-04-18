57c011bd947a4cfdec0f08b75dfa1fecc1d9268d
Yousef Qandeel <yqandil7@gmail.com>
2022-04-18 23:07:46 +0300

    final add

---
+++ stack queue animal shelter/StackQueueAnimalShelter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace StackQueueAnimalShelter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+            Node<int> node1 = new Node<int>(55);
+            Node<int> node2 = new Node<int>(4);
+            //stack.Push(node1);
+            //stack.Push(node2);
+            //stack.Push(node1);
+            //stack.Push(node2);
+            Queue<int> queue = new Queue<int>();
+            //queue.Enqueue(node1);
+            //queue.Dequeue();
+            //queue.Dequeue();
+            //queue.Enqueue(node2.Value);
+            //Console.WriteLine(queue.Peek());
+            //Console.WriteLine(queue.IsEmpty());
+            //Console.WriteLine(stack.Peek());
+            //Console.WriteLine(stack.IsEmpty());
+
+            PseudoQueue<int> pseudoQueue = new PseudoQueue<int>();
+
+            pseudoQueue.Enqueue(20);
+            pseudoQueue.Enqueue(15);
+            pseudoQueue.Enqueue(10);
+            pseudoQueue.Enqueue(5);
+
+            Console.WriteLine(pseudoQueue.Dequeue().Value);
+        }
+    }
+}
