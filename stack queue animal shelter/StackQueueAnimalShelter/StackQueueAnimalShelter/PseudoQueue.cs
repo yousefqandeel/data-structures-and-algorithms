2a7d29f9c675f3f979438d6a216bbcd6a6862e2a
Yousef Qandeel <yqandil7@gmail.com>
2022-04-19 04:41:04 +0300

    final addition

---
+++ stack queue animal shelter/StackQueueAnimalShelter/StackQueueAnimalShelter/PseudoQueue.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackQueueAnimalShelter
+{
+    public class PseudoQueue<T>
+    {
+        public Stack<T> s1 = new Stack<T>();
+        public Stack<T> s2 = new Stack<T>();
+
+        public void Enqueue(T node)
+        {
+            s1.Push(node);
+        }
+
+        public Node<T> Dequeue()
+        {
+            while (!s1.IsEmpty())
+            {
+                s2.Push(s1.Pop().Value);
+            }
+
+            Node<T> temp = s2.Pop();
+
+            while (!s1.IsEmpty())
+            {
+                s1.Push(s2.Pop().Value);
+            }
+
+            return temp;
+        }
+    }
+}
---
+++ stack queue animal shelter/StackQueueAnimalShelter/StackQueueAnimalShelter/Stack.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackQueueAnimalShelter
+{
+    public class Stack<T>
+    {
+        public Node<T> Top { get; set; }
+
+        public Stack()
+        {
+            Top = null;
+        }
+
+        public T Peek()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("The stack is empty");
+                return default;
+            }
+            else
+            {
+                T top = Top.Value;
+                return top;
+            }
+        }
+
+        public Node<T> Pop()
+        {
+            Node<T> temp = Top;
+            Top = Top.next;
+            temp.next = null;
+            return temp;
+        }
+
+        public void Push(T node)
+        {
+            Node<T> temp = new Node<T>(node);
+            temp.next = Top;
+            Top = temp;
+        }
+        public bool IsEmpty()
+        {
+            return (Top == null);
+        }
+    }
+}
